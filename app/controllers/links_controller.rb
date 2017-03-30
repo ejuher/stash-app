376e902c2fd97222399c28c9018a14017387414c
Erich Uher <ejuher@gmail.com>
2017-03-30 10:56:09 -0700

    deserialize params and add create action in link controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,8 +7,20 @@ class LinksController < ApplicationController
   end
 
   def create
+    @link = Link.new(link_params)
+    if @link.save
+      render json: @link
+    else
+      render json: { errors: @link.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def link_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: %i(url title author))
   end
 end
