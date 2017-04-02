99230a43696e9c1108c9f5e7735e91efe1ca2da7
Erich Uher <ejuher@gmail.com>
2017-04-02 01:23:04 -0700

    add goToLink action to links-table

--- frontend/app/components/links-table.js
+++ frontend/app/components/links-table.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['row']
+  classNames: ['row'],
+
+  actions: {
+    goToLink(link) {
+      document.location.assign(link.get('url'));
+    }
+  }
 });
