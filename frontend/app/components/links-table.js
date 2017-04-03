bbb9d794b061553afc5183b82abf3d80298f9573
Erich Uher <ejuher@gmail.com>
2017-04-02 21:26:46 -0700

    sort links by createdAt

--- frontend/app/components/links-table.js
+++ frontend/app/components/links-table.js
@@ -3,6 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['row'],
 
+  sortedLinks: Ember.computed.sort('links', function(a, b) {
+    // b.createdAt is not defined at the instant the link is created
+    if (b.get('createdAt') === undefined || b.get('createdAt').getTime() > a.get('createdAt').getTime()) {
+      // b comes before a
+      return 1; 
+    } else { 
+      // a comes before b
+      return -1;
+    }
+    return 0;
+  }),
+
   actions: {
     goToLink(link) {
       document.location.assign(link.get('url'));
