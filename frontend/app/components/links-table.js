be3c7d1a9c52c59b918b0c207df36644ea7f74f8
Erich Uher <ejuher@gmail.com>
2017-04-22 11:25:03 -0700

    add tagEmoji to link

--- frontend/app/components/links-table.js
+++ frontend/app/components/links-table.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['row'],
-
   sortedLinks: Ember.computed.sort('links', function(a, b) {
     // b.createdAt is not defined at the instant the link is created
     if (b.get('createdAt') === undefined || b.get('createdAt').getTime() > a.get('createdAt').getTime()) {
--- frontend/app/models/link.js
+++ frontend/app/models/link.js
@@ -4,5 +4,18 @@ export default DS.Model.extend({
   url: DS.attr('string'),
   title: DS.attr('string'),
   tag: DS.attr('string'),
-  createdAt: DS.attr('date', { serialize: false }) // not working :<
+  createdAt: DS.attr('date', { serialize: false }), // not working :<
+
+  tagEmoji: Em.computed('tag', function() {
+    let tag = this.get('tag')
+    if (tag === 'AUDIO') {
+      return '🎷'
+    } else if (tag === 'VIDEO') {
+      return '📼'
+    } else if (tag === 'ARTICLE') {
+      return '📰'
+    } else if (tag === 'OTHER') {
+      return '🤷‍'
+    }
+  })
 });
